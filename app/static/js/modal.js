bd41af5f64b99563b78d3553adcb7e09361e8af2
Christopher Gibilisco <cjgibilisco@gmail.com>
2026-06-10 23:34:49 -0500

    fixed x button for CSP

--- app/static/js/modal.js
+++ app/static/js/modal.js
@@ -1,3 +1,7 @@
 document.body.addEventListener("wishCreated", function () {
 	document.getElementById("modal").innerHTML = "";
 });
+
+document.body.addEventListener("modalClosed", function () {
+	document.getElementById("modal").innerHTML = "";
+});
